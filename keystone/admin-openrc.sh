df58af35eebfd60e0526de6a6379e4efebf04ba8
Pierre Garel <pierre@garel.net>
2016-03-21 15:14:58 +0100

    Initial Commit

---
+++ keystone/admin-openrc.sh
@@ -0,0 +1,10 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=admin
+export OS_AUTH_URL=http://controller:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\[\e[0m\]($OS_USERNAME)$ \[\033[s\]"
